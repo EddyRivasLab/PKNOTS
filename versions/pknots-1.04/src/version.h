0e8ebfdb848e00d568c37c1a5bb6073fd8e07188
Elena Rivas <erivas@dhcp-10-250-202-136.harvard.edu>
2020-01-30 11:47:32 -0500

    small compilation changes

--- versions/pknots-1.04/src/version.h
+++
@@ -1,7 +0,0 @@
-/* version.h
- * 
- */
-
-#define RELEASE   "1.04"    
-#define RELEASEDATE "Fri Jul  9 14:23:04 CDT 2004"  
-
--- versions/pknots-1.06/src/version.h
+++
@@ -1,7 +0,0 @@
-/* version.h
- * 
- */
-
-#define RELEASE   "1.06"    
-#define RELEASEDATE "MAY 2008"  
-
